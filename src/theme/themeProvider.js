966cb5b73f80dd7acc54e1896bb36c7a335dc6d1
dangkhoa105 <dangkhoa.dk9@gmail.com>
2020-11-08 23:19:30 +0700

    temp add color

---
+++ src/theme/themeProvider.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { darkTheme, lightTheme } from './theme'
+
+export const AppThemeContext = React.createContext({
+    theme: 'light',
+    toggle: () => { },
+    colors: undefined,
+})
+const THEME_KEY = 'THEME'
+
+export const useThemes = () => {
+    const themeContext = useContext(AppThemeContext)
+    const toggle = () => {
+        themeContext.toggle()
+    }
+
+    const theme = useTheme()
+    return {
+        toggle,
+        ...theme,
+        colors: theme.colors,
+        theme: themeContext.theme,
+    }
+}
+
+export const AppThemeProvider = (props) => {
+    const { children } = props
+    const [currentTheme, setTheme] = React.useState('light')
+
+    React.useEffect(() => {
+        const boostrap = async () => {
+            const themePersist = await storage.loadString(THEME_KEY)
+            if (themePersist) setTheme(themePersist)
+        }
+
+        boostrap()
+    }, [])
+
+    const toggle = () => {
+        const nextTheme = currentTheme === 'dark' ? 'light' : 'dark'
+        setTheme(nextTheme)
+        storage.saveString('THEME', nextTheme)
+    }
+
+
+    return (
+        <>
+            <AppThemeContext.Provider value={{ theme: currentTheme, toggle, colors: undefined }}>
+                {children}
+            </AppThemeContext.Provider>
+        </>
+    )
+}
