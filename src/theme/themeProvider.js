c192cdfbdc73bd98794708618390aae6318ee411
dinhmai74 <anh.dinh74@gmail.com>
2020-11-19 22:28:18 +0700

    Update color font

--- src/theme/themeProvider.js
+++ src/theme/themeProvider.js
@@ -1,53 +1,56 @@
-import React from 'react'
-import { darkTheme, lightTheme } from './theme'
+import {useTheme} from '@shopify/restyle';
+import React, {useContext} from 'react';
+import {darkTheme, lightTheme} from './theme';
 
 export const AppThemeContext = React.createContext({
-    theme: 'light',
-    toggle: () => { },
-    colors: undefined,
-})
-const THEME_KEY = 'THEME'
+  theme: 'light',
+  toggle: () => {},
+  colors: undefined,
+});
+const THEME_KEY = 'THEME';
 
 export const useThemes = () => {
-    const themeContext = useContext(AppThemeContext)
-    const toggle = () => {
-        themeContext.toggle()
-    }
+  const themeContext = useContext(AppThemeContext);
+  const toggle = () => {
+    themeContext.toggle();
+  };
 
-    const theme = useTheme()
-    return {
-        toggle,
-        ...theme,
-        colors: theme.colors,
-        theme: themeContext.theme,
-    }
-}
+  const theme = useTheme();
+  return {
+    toggle,
+    ...theme,
+    colors: theme.colors,
+    theme: themeContext.theme,
+  };
+};
 
 export const AppThemeProvider = (props) => {
-    const { children } = props
-    const [currentTheme, setTheme] = React.useState('light')
+  const {children} = props;
+  const [currentTheme, setTheme] = React.useState('light');
 
-    React.useEffect(() => {
-        const boostrap = async () => {
-            const themePersist = await storage.loadString(THEME_KEY)
-            if (themePersist) setTheme(themePersist)
-        }
+  React.useEffect(() => {
+    const boostrap = async () => {
+      //   const themePersist = await storage.loadString(THEME_KEY);
+      //   if (themePersist) {
+      //     setTheme(themePersist);
+      //   }
+    };
 
-        boostrap()
-    }, [])
+    boostrap();
+  }, []);
 
-    const toggle = () => {
-        const nextTheme = currentTheme === 'dark' ? 'light' : 'dark'
-        setTheme(nextTheme)
-        storage.saveString('THEME', nextTheme)
-    }
+  const toggle = () => {
+    const nextTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    setTheme(nextTheme);
+    // storage.saveString('THEME', nextTheme);
+  };
 
-
-    return (
-        <>
-            <AppThemeContext.Provider value={{ theme: currentTheme, toggle, colors: undefined }}>
-                {children}
-            </AppThemeContext.Provider>
-        </>
-    )
-}
+  return (
+    <>
+      <AppThemeContext.Provider
+        value={{theme: currentTheme, toggle, colors: undefined}}>
+        {children}
+      </AppThemeContext.Provider>
+    </>
+  );
+};
