b17b109ca5988b049c0650700ec78eaccd2114e7
dangkhoa105 <dangkhoa.dk9@gmail.com>
2020-11-21 16:16:05 +0700

    commit

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-import React, {useState, useEffect} from 'react';
-import {View, Text, FlatList, StyleSheet} from 'react-native';
-import ItemPlan from './Customs/ItemPlan';
-=======
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, FlatList, StyleSheet } from 'react-native'
 import ItemPlan from './Customs/ItemPlan'
->>>>>>> 030bb1430744181b86eb7fd9f4ba797a26fe1faa
 
 const DataRatio = {
   content: "You're almost done, go ahead!",
   ratio: '80',
-<<<<<<< HEAD
-};
-=======
 }
->>>>>>> 030bb1430744181b86eb7fd9f4ba797a26fe1faa
 
 const DataPlan = [
   {
@@ -58,23 +48,22 @@ const DataPlan = [
     content: 'Complete today to have the first streak',
     isSelected: false,
   },
-<<<<<<< HEAD
-];
+]
 
 export default function HomeScreen() {
-  const [ratio, setRatio] = useState(0);
+  const [ratio, setRatio] = useState(0)
 
   useEffect(() => {
-    calRatio();
-  }, [ratio]);
+    calRatio()
+  }, [ratio])
 
   const calRatio = () => {
     let listComplete = DataPlan.filter((val) => {
-      return val.isSelected === true;
-    });
+      return val.isSelected === true
+    })
 
-    setRatio((listComplete.length * 100) / DataPlan.length);
-  };
+    setRatio((listComplete.length * 100) / DataPlan.length)
+  }
 
   return (
     <View style={styles.container}>
@@ -88,55 +77,21 @@ export default function HomeScreen() {
 
         {/* RATIO */}
         <View style={styles.ratioHeader}>
-          <View style={[styles.ratioInfo, {width: `${ratio}%`}]} />
+          <View style={[styles.ratioInfo, { width: `${ratio}%` }]} />
         </View>
       </View>
 
-=======
-]
-
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      {/* HEADER */}
-      <View style={styles.header}>
-        {/* TITLE HEADER */}
-        <View style={styles.titleHeader}>
-          <Text style={styles.txtTitleHeader}>{DataRatio.content}</Text>
-          <Text style={styles.txtTitleHeader}>{DataRatio.ratio}%</Text>
-        </View>
-
-        {/* RATIO */}
-        <View style={styles.ratioHeader}>
-          <View style={[styles.ratioInfo, { width: `${DataRatio.ratio}%` }]} />
-        </View>
-      </View>
-
-      {/* <View style={styles.divider} /> */}
-
->>>>>>> 030bb1430744181b86eb7fd9f4ba797a26fe1faa
       {/* CONTENT */}
-      <View style={styles.content}>
-        <FlatList
-          data={DataPlan}
-          showsVerticalScrollIndicator={false}
-          keyExtractor={(item, index) => 'key' + index}
-<<<<<<< HEAD
-          renderItem={({item, index}) => {
-            return <ItemPlan data={item} />;
-=======
-          renderItem={({ item, index }) => {
-            return <ItemPlan data={item} />
->>>>>>> 030bb1430744181b86eb7fd9f4ba797a26fe1faa
-          }}
-        />
-      </View>
+      <FlatList
+        data={DataPlan}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item, index) => 'key' + index}
+        renderItem={({ item, index }) => {
+          return <ItemPlan data={item} />
+        }}
+      />
     </View>
-<<<<<<< HEAD
-  );
-=======
   )
->>>>>>> 030bb1430744181b86eb7fd9f4ba797a26fe1faa
 }
 
 const styles = StyleSheet.create({
@@ -148,18 +103,13 @@ const styles = StyleSheet.create({
     paddingHorizontal: 22,
   },
   header: {
-    flex: 1,
+    paddingVertical: 16,
   },
   titleHeader: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingHorizontal: 7,
-    paddingVertical: 16,
-<<<<<<< HEAD
     paddingBottom: 1,
-=======
-    paddingBottom: 12,
->>>>>>> 030bb1430744181b86eb7fd9f4ba797a26fe1faa
   },
   txtTitleHeader: {
     fontSize: 12,
@@ -177,19 +127,5 @@ const styles = StyleSheet.create({
     height: 8,
     borderRadius: 16,
   },
-<<<<<<< HEAD
-  content: {
-    flex: 4,
-  },
-});
-=======
-  divider: {
-    height: 0.5,
-    backgroundColor: '#91A7AD',
-    marginVertical: 16,
-  },
-  content: {
-    flex: 4,
-  },
+  content: {},
 })
->>>>>>> 030bb1430744181b86eb7fd9f4ba797a26fe1faa
